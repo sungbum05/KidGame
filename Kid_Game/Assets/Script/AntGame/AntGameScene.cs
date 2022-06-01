3f17066ef893d4d2eb804daf65637c65242132ed
sungbum05 <sblim05@naver.com>
2022-06-01 23:25:17 +0900

    AntGame/DuckGame

---
+++ Kid_Game/Assets/Script/AntGame/AntGameScene.cs
@@ -0,0 +1,201 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AntGameScene : MonoBehaviour
+{
+    [SerializeField]
+    int SelectNum;
+    [SerializeField]
+    int PickNum;
+    [SerializeField]
+    bool StartChk = false;
+    [SerializeField]
+    int MaxCount = 5;
+    [SerializeField]
+    int CurCount = 0;
+
+    [Space(10)]
+    [SerializeField]
+    private Vector2 EnterPos; // 개미 생성 지점
+    [SerializeField]
+    private Vector2 StayPos; // 개미 머무는 지점
+    [SerializeField]
+    private Vector2 ExitPos; // 개미 나가는 지점
+
+    [Space(10)]
+    [SerializeField]
+    private GameObject Breads;
+    [SerializeField]
+    private Vector2 BreadStayPos;
+    [SerializeField]
+    private Vector2 BreadExitPos;
+    [SerializeField]
+    private List<GameObject> Bread;
+    [SerializeField]
+    private List<GameObject> BreadPos;
+    [SerializeField]
+    private float ShowTime = 1.0f;
+
+    [Space(10)]
+    [SerializeField]
+    LayerMask layerMask;
+    Vector2 MousePos;
+
+    [Space(10)]
+    [SerializeField]
+    private List<GameObject> AntGroup;
+    [SerializeField]
+    GameObject Ants;
+    [SerializeField]
+    BoxCollider2D AntZone;
+    [SerializeField]
+    Sprite AntChangeImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MouseClick();
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            MouseUp();
+        }
+    }
+
+    IEnumerator StartGame()
+    {
+        yield return null;
+        Destroy(Ants);
+        Ants = null;
+
+        StartCoroutine(EnterAnt());
+        StartCoroutine(BreadPosChange());
+
+        CurCount++;
+    }
+
+    #region  마우스 상호작용 함수들
+    void MouseClick() // 마우스를 누르고 있는동안 ray실행
+    {
+        MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(MousePos, transform.forward, 10.0f, layerMask);
+        if (hit)
+        {
+            string[] SpiltName = hit.collider.name.Split('_');
+            PickNum = int.Parse(SpiltName[1]);
+            hit.collider.gameObject.transform.position = MousePos;
+        }
+    }
+
+    void MouseUp()
+    {
+        if((Mathf.Abs(MousePos.x) < Mathf.Abs(AntZone.bounds.extents.x) && Mathf.Abs(MousePos.y) < Mathf.Abs(AntZone.bounds.extents.y)) && SelectNum == PickNum)
+        {
+            Debug.Log("Yes");
+            foreach(Transform Child in Ants.transform)
+            {
+                Child.gameObject.GetComponent<SpriteRenderer>().sprite = AntChangeImg;
+                Bread[PickNum - 1].SetActive(false);
+            }
+
+            StartCoroutine(ExitAnt());
+        }
+
+        else
+        {
+            Bread[PickNum - 1].transform.DOMove(BreadPos[PickNum - 1].transform.position, ShowTime / 2);
+            PickNum = 0;
+        }
+    }
+    #endregion
+
+    #region 개미 연출
+    IEnumerator EnterAnt()
+    {
+        yield return null;
+        GetShuffleList<GameObject>(AntGroup);
+        string[] SplitName = AntGroup[0].name.Split('_');
+        SelectNum = int.Parse(SplitName[1]);
+
+        Ants = Instantiate(AntGroup[0], EnterPos, Quaternion.identity);
+        Ants.transform.DOMove(StayPos, ShowTime * 1.5f);
+
+        yield return new WaitForSeconds(ShowTime * 2.0f);
+    }
+
+    IEnumerator ExitAnt()
+    {
+        yield return null;
+        Ants.transform.DOMove(ExitPos, ShowTime * 2.0f);
+
+        yield return new WaitForSeconds(ShowTime * 2);
+
+        StartCoroutine(StartGame());
+    }
+    #endregion
+
+    IEnumerator BreadPosChange() // 빵의 위치를 바꿔준다(연출 포함)
+    {
+        yield return null;
+
+        if (StartChk == true)
+        {
+            Bread[PickNum - 1].transform.position = BreadPos[PickNum - 1].transform.position;
+            Breads.transform.DOMove(BreadExitPos, ShowTime);
+
+            yield return new WaitForSeconds(ShowTime);
+        }
+
+        int i = 0;
+
+        GetShuffleList<GameObject>(BreadPos);
+
+        foreach(Transform Child in Breads.transform)
+        {
+            Child.transform.position = new Vector2(BreadPos[i].transform.position.x, Child.transform.position.y);
+            string[] SplitName = BreadPos[i].name.Split('_');
+            Child.gameObject.GetComponent<Bread>().Pos = int.Parse(SplitName[1]);
+            i++;
+        }
+
+        if (StartChk == true)
+        {
+            Debug.Log("asdad");
+            Bread[PickNum - 1].SetActive(true);
+            PickNum = 0;
+
+            yield return new WaitForSeconds(ShowTime / 2);       
+        }
+
+        Breads.transform.DOMove(BreadStayPos, ShowTime);
+
+        StartChk = true;
+        i = 0;
+    }
+
+    public List<T> GetShuffleList<T>(List<T> _list) // 제네릭 리스트를 이용한 리스트 랜덤 셔플 함수
+    {
+        for (int i = _list.Count - 1; i > 0; i--)
+        {
+            int rnd = Random.Range(0, i + 1); // 1 추가 해서 첫번 째 오브젝트도 첫번째로 나올 수 있도록 수정함
+
+            T temp = _list[i];
+            _list[i] = _list[rnd];
+            _list[rnd] = temp;
+        }
+
+        return _list;
+    }//(출처 : https://drehzr.tistory.com/802)
+}
---
+++ Kid_Game/Assets/Script/DuckGame/DuckGameMgr.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DuckGameMgr : MonoBehaviour
+{
+    [SerializeField]
+    private string SelectColor; // 선택된 컬러 받아올 변수
+
+    bool StartChk = false;
+
+    [SerializeField]
+    int MaxGameCount = 5;
+
+    [SerializeField]
+    int CurGameCount = 0;
+
+    [SerializeField]
+    private List<string> Color; // 컬러 챗박스에 색깔을 배정할 컬러
+
+    [SerializeField]
+    private SpriteRenderer ChatBox; // 오리들에게 색깔을 배정할 컬러
+
+    [SerializeField]
+    private List<Sprite> ColorChatBox; // 엄마 오리가 부를 컬러 이미지들
+
+    public Dictionary<string, Sprite> ColorChatBoxkDic = new Dictionary<string, Sprite>();
+
+
+    [SerializeField]
+    private List<Button> BabyDuckBtns; // 애기 오리들 상호 작용 버튼
+
+    private void Awake()
+    {
+        DicSetting();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartGame());
+
+        foreach (Button Btn in BabyDuckBtns)
+        {
+            Btn.onClick.AddListener(() =>
+            {
+                if (Btn.GetComponent<BabyDuckInfo>().BabyColor == SelectColor)
+                {
+                    StartCoroutine(StartGame());
+                    Debug.Log(Btn.name);
+                }
+            });
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartGame()
+    {
+        yield return null;
+
+        if(StartChk == true)
+        {
+            StartCoroutine(ExitDuck());
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        StartChk = true;
+        GetShuffleList<string>(Color);
+        SettingBabyDuck();
+        SettingColorChatBox();
+
+        CurGameCount+=1;
+    }
+
+    IEnumerator ExitDuck()
+    {
+        yield return null;
+
+        foreach (Button Btn in BabyDuckBtns)
+        {
+            Btn.gameObject.GetComponent<Image>().DOFade(0, 1);
+        }
+
+        yield return new WaitForSeconds(1.4f);
+
+        StartCoroutine(EnterDuck());
+    }
+
+    IEnumerator EnterDuck()
+    {
+        yield return null;
+
+        foreach (Button Btn in BabyDuckBtns)
+        {
+            Btn.gameObject.GetComponent<Image>().DOFade(1, 1);
+        }
+    }
+
+    void DicSetting()
+    {
+        foreach (Sprite Img in ColorChatBox)
+        {
+            string[] NameSplit = Img.name.Split('_');
+            ColorChatBoxkDic.Add(NameSplit[1], Img);
+        }
+    } // Dic세팅
+
+    private void SettingBabyDuck()
+    {
+        int i = 0;
+
+        foreach(Button Btn in BabyDuckBtns)
+        {
+            Btn.GetComponent<BabyDuckInfo>().BabyColor = Color[i];
+            Btn.GetComponent<BabyDuckInfo>().ColorSetting();
+            i++;
+        }
+
+        i = 0;
+    } // 아기 오리들 색깔 변경
+
+    private void SettingColorChatBox()
+    {
+        int SelectColorIdx = Random.Range(0, 3);
+        SelectColor = Color[SelectColorIdx];
+
+        ChatBox.sprite = ColorChatBoxkDic[SelectColor];
+    } // 챗박스 색깔 변경
+
+    public List<T> GetShuffleList<T>(List<T> _list) // 제네릭 리스트를 이용한 리스트 랜덤 셔플 함수
+    {
+        for (int i = _list.Count - 1; i > 0; i--)
+        {
+            int rnd = Random.Range(0, i + 1); // 1 추가 해서 첫번 째 오브젝트도 첫번째로 나올 수 있도록 수정함
+
+            T temp = _list[i];
+            _list[i] = _list[rnd];
+            _list[rnd] = temp;
+        }
+
+        return _list;
+    }//(출처 : https://drehzr.tistory.com/802) // 수정: sblim05
+}
